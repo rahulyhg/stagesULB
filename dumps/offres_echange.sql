48169eba6206f65a4a0b9247e1d07867c336f2af
Martin Fontaine <marti.fontaine@gmail.com>
2017-06-14 22:55:05 +0200

    table offres d'échange

---
+++ dumps/offres_echange.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 14 Juin 2017 à 20:54
+-- Version du serveur :  5.7.14
+-- Version de PHP :  7.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `stages`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offres_echange`
+--
+
+CREATE TABLE `offres_echange` (
+  `id` bigint(20) NOT NULL,
+  `hospital_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `datetime` varchar(255) NOT NULL,
+  `commentaire` text NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `offres_echange`
+--
+ALTER TABLE `offres_echange`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `offres_echange`
+--
+ALTER TABLE `offres_echange`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
